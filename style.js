f0eb51ffed5e3b6b13e27cccb535c66b2be11cb0
Shravanb-nd <shravan.bhagavath@netradyne.com>
2022-01-22 00:31:15 +0530

    added_navbar_and_other_changes

--- style.js
+++ style.js
@@ -19,27 +19,14 @@ function topFunction() {
 }
 
 
-// LOGO SLIDER JQUERY
+// NAVBAR
 
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
+$(document).ready(function () {
+if ($(window).width() > 991){
+$('.navbar-light .d-menu').hover(function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+    }, function () {
+        $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
     });
+    }
 });
